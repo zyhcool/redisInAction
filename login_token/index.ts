938f8c02fc518e83cad059a49d747828a9bf1ddd
zyh <13105027620@163.com>
2019-09-17 15:10:31 +0800

    df

--- login_token/index.ts
+++ login_token/index.ts
@@ -1,6 +1,12 @@
 import { redisClient } from "../app";
+import { createHash } from "crypto";
 
+// tokens and userIds
 const HashTitle = "tokens";
+// recent login-in tokens
+const RECENT = "recents";
+// views of items
+const VIEWS = "views";
 
 const storeToken = (userId: string, token: string) => {
     redisClient.hset(HashTitle, token, userId);
@@ -19,10 +25,74 @@ const checkToken = (token: string) => {
 const updateToken = (token: string, userId: string, itemId?: string) => {
     const time = new Date().getTime();
     redisClient.hset(HashTitle, token, userId);
-    redisClient.zadd("recents", token, time);
+    redisClient.zadd(RECENT, token, time);
     if (itemId) {
-        redisClient.zadd("views|" + token, itemId, time);
-        redisClient.zremrangebyrank("views|"+token,0,-26);
+        redisClient.zadd(VIEWS + "|" + token, itemId, time);
+        redisClient.zremrangebyrank(VIEWS + "|" + token, 0, -26);
     }
 }
 
+const cleanToken = () => {
+    redisClient.zcard(RECENT, (err, size) => {
+        if (size <= 10 * 5) {
+            return;
+        } else {
+            redisClient.zrange(RECENT, 0, 100, (err, value) => {
+                const views_tokens = value.map((token) => {
+                    return `${VIEWS}|${token}`;
+                });
+                const cart_tokens = value.map((token) => {
+                    return `${CART}|${token}`;
+                })
+                redisClient.del([...views_tokens, ...cart_tokens]);
+                redisClient.zrem(RECENT, value);
+                redisClient.hdel(HashTitle, value);
+            })
+        }
+    });
+}
+
+
+const CART = "carts";
+// shopping cart
+const addItemToCart = (token: string, itemId: string, num: number) => {
+    redisClient.hset(CART + "|" + token, itemId, `${num}`);
+}
+
+const REQUESTCACHE = "requestCache";
+
+const cacheRequest = (request: string, cb) => {
+    const requestKey = REQUESTCACHE + createHash("sha256").update(JSON.stringify(request));
+    redisClient.get(requestKey, (err, value) => {
+        if (!value) {
+            const content = cb(request);
+            redisClient.setex(REQUESTCACHE, 300000, content);
+        }
+    });
+}
+
+const DELAY = "delay";
+const SCHEDULE = "schedule";
+
+const rowCacheDelay = (id: string, delay: number) => {
+    redisClient.zadd(DELAY, delay);
+    redisClient.zadd(SCHEDULE, id, new Date().getTime());
+}
+
+const cacheRows = () => {
+    let end = false;
+    while (!end) {
+        redisClient.zrange(SCHEDULE, 0, 0, (err, value) => {
+            const id = value[0];
+            redisClient.zscore(DELAY, id, (err, value) => {
+                if (Number.parseInt(value, 10) <= 0) {
+                    redisClient.zrem(DELAY, id);
+                    redisClient.zrem(SCHEDULE, id);
+                    redisClient.del("rows:" + id);
+                }
+            })
+        })
+    }
+}
+
+redisClient.persist
